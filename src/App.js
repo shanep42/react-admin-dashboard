ace77fd6ac7bcd67fa681c0fe8c8dc9e2458bec8
Shane <shanep42@gmail.com>
2023-03-15 17:13:58 -0500

    Working on scenes/team

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Dashboard from './scenes/dashboard';
-// import Sidebar from '.scenes/global/Sidebar';
 import Sidebar from "./scenes/global/Sidebar";
-// import Team from '.scenes/team';
+import Team from '.scenes/team';
 // import Invoices from '.scenes/invoices';
 // import Contacts from '.scenes/contacts';
 // import Bar from '.scenes/bar';
@@ -22,8 +21,8 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Sidebar />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
             <Routes>
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,14 @@
+import Header from "../../components/Header"
+import { Box } from "@mui/system"
+
 function Dashboard() {
-    return (
-      <div>Dashboard</div>
-    )
-  }
-  
-  export default Dashboard
+  return (
+    <Box m='20px'>
+      <Box display='flex' justifyContent='space-between' alignItems='center'>
+        <Header title='DASHBOARD' subtitle='Welcome to your Dashboard' />
+      </Box>
+    </Box>
+  )
+}
+
+export default Dashboard
