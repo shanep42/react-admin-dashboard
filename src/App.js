ee751eb686aa09712e804a9f4415b6a20fc4f4a4
Shane <shanep42@gmail.com>
2023-03-15 20:00:41 -0500

    Refactors a contact scene from the teams scene

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Sidebar from "./scenes/global/Sidebar";
 import Team from './scenes/team';
 import { useState } from "react";
 // import Invoices from '.scenes/invoices';
-// import Contacts from '.scenes/contacts';
+import Contacts from './scenes/contacts';
 // import Bar from '.scenes/bar';
 // import Form from '.scenes/Form';
 // import Line from '.scenes/line';
@@ -31,7 +31,7 @@ function App() {
             <Routes>
               <Route path='/' element={<Dashboard />} />
               <Route path='/team' element={<Team />} />
-              {/* <Route path='/contacts' element={<Contacts />} /> */}
+              <Route path='/contacts' element={<Contacts />} />
               {/* <Route path='/invoices' element={<Invoices />} /> */}
               {/* <Route path='/form' element={<Form />} /> */}
               {/* <Route path='/bar' element={<Bar />} /> */}
--- src/scenes/contacts/index.jsx
+++ src/scenes/contacts/index.jsx
@@ -1,50 +1,30 @@
-import { Box, Typography, useTheme } from "@mui/system";
-import { DataGrid } from "@mui/x-data-grid";
+import { Box } from "@mui/system";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { tokens } from '../../theme';
-import { mockDataTeam } from '../../data/mockData';
-import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
-import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
-import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
-import Header from '../../components/Header';
+import { mockDataContacts } from '../../data/mockData';
 
-const Team = () => {
+import Header from '../../components/Header';
+import { useTheme } from '@mui/material';
+
+const Contacts = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
     const columns = [
-        { field: 'id', headerName: 'ID' },
+        { field: 'id', headerName: 'ID', flex: 0.5 },
+        { field: 'registrarId', headerName: 'Registrar ID' },
         { field: 'name', headerName: 'Name', flex: 1, cellClassName: 'name-column--cell' },
         { field: 'age', headerName: 'Age', type: 'number', headerAlign: 'left', align: 'left' },
         { field: 'phone', headerName: 'Phone Number', flex: 1 },
         { field: 'email', headerName: 'Email', flex: 1 },
-        {
-            field: 'access', headerName: 'Access Level', flex: 1, renderCell: ({ row: { access } }) => {
-                return (
-                    <Box
-                        width='60%'
-                        m='0 auto'
-                        p='5px'
-                        display='flex'
-                        justifyContent='center'
-                        backgroundColor={
-                            access === 'admin'
-                                ? colors.greenAccent[600]
-                                : colors.greenAccent[700]
-                        }
-                        borderRadius='4px'
-                    >
-                        {access === 'admin' && <AdminPanelSettingsOutlinedIcon />}
-                        {access === 'manager' && <SecurityOutlinedIcon />}
-                        {access === 'user' && <LockOpenOutlinedIcon />}
-                    </Box >
-                )
-            }
-        },
+        { field: 'address', headerName: 'Adress' },
+        { field: 'city', headerName: 'City' },
+        { field: 'zipCode', headerName: 'Zip Code' },
     ]
 
     return (
         <Box m='20px'>
-            <Header title='TEAM' subtitle='Managing the Team Members' />
+            <Header title='CONTACTS' subtitle='List of Contacts for Future Reference' />
             <Box
                 m="40px 0 0 0"
                 height="75vh"
@@ -72,15 +52,19 @@ const Team = () => {
                     "& .MuiCheckbox-root": {
                         color: `${colors.greenAccent[200]} !important`,
                     },
+                    '& .MuiDataGrid-toolbarContainer .MuiButton-text': {
+                        color: `${colors.grey[100]} !important`
+                    }
                 }}
             >
                 <DataGrid
-                    rows={mockDataTeam}
+                    rows={mockDataContacts}
                     columns={columns}
+                    components={{ Toolbar: GridToolbar }}
                 />
             </Box>
         </Box>
     )
 }
 
-export default Team;
+export default Contacts;
