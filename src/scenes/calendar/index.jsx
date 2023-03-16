143882806deaa03c9d65d18d9b789c2f153ff448
shanep42 <107292088+shanep42@users.noreply.github.com>
2023-03-16 00:22:05 -0500

    Begins calendar page using Full Calender package

---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { FullCalendar, formatDate } from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+import { Box, List, ListItem, ListItemText, Typography, useTheme } from '@mui/material';
+import { tokens } from '../../theme'
+import Header from '../../components/Header';
+
+const Calendar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [currentEvents, setCurrentEvents] = useState([]);
+
+    const handleDateClick = (selected) => {
+        // TODO: Use a modal instead of prompt
+        const title = prompt('Please enter a new title for your event');
+        // Just checking out full calendar
+        console.log('selection info provided by fullCalendar:', selected);
+        const calendarApi = selected.view.calendar;
+        calendarApi.unselect();
+
+        if (title) {
+            calendarApi.addEvent({
+                id: `${selected.date}-${title}`,
+                title,
+                start: selected.startStr,
+                end: selected.endStr,
+                allDay: selected.allDay
+            })
+        }
+    }
+
+    const handleEventClick = (selected) => {
+        if (window.confirm(`Are you sure you want to delete the event ${selected.event.title}?`)) {
+            selected.event.remove();
+        }
+    }
+
+    return (
+        <Box m='20px'>
+            <Header title='CALENDAR' subtitle='Full Calendar Interactive Page' />
+            <Box display='flex' justifyContent='space-between'>
+                {/* Calendar Sidebar */}
+                <Box flex='1 1 20%' backgroundColor={colors.primary[400]} p='15px' borderRadius='4px'>
+                    <Typography variant='h5'>Events</Typography>
+                    <List>
+                        {currentEvents.map((event) => {
+                            <ListItem
+                                key={event.id}
+                                sx={{ backgroundColor: colors.greenAccent[500], margin: '10px 0', borderRadius: '2px' }}
+                            >
+                                <ListItemText
+                                    primary={event.title}
+                                    secondary={
+                                        <Typography>
+                                            {formatDate(event.start, {
+                                                year: 'numeric',
+                                                month: 'short',
+                                                day: 'numeric'
+                                            })}
+                                        </Typography>
+                                    }
+                                />
+                            </ListItem>
+                        })}
+                    </List>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default Calendar
